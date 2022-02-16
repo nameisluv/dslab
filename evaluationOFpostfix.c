d00c67a6aac1b5d4047cc370df19759c1a3f3a13
Luv kumar <91609093+nameisluv@users.noreply.github.com>
2022-02-16 18:40:59 +0530

    Update and rename evaluation.c to evaluationOFpostfix.c

---
+++ evaluationOFpostfix.c
@@ -0,0 +1,73 @@
+//Refer Postfix_Eval_Algorithm.txt 
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int top = -1;
+double stack[50];
+void push(double item)
+{
+    stack[++top] = item;
+}
+
+double pop()
+{
+    double item;
+    item = stack[top--];
+    return item;
+}
+
+double evaluate(double op1, double op2, char symbol)
+{
+    switch(symbol){
+        case '+':
+            return op1 + op2;
+        case '-':
+            return op1 - op2;
+        case '*':
+            return op1 * op2;
+        case '/':
+            return op1 / op2;
+        case '%':
+            return (int)op1 % (int)op2;
+        case '^':
+        case '$':
+            return pow(op1,op2);
+    }
+}
+
+int isDigit(char symbol)
+{
+	return (symbol >= '0' && symbol <= '9');
+}
+
+void main()
+{
+    char expression[50], symbol;
+    double res, op1, op2;
+
+    printf("\nEnter a valid postfix expression (avoid whitespaces): ");
+    scanf("%s",expression);
+
+    int length = strlen(expression);
+
+    for(int i = 0; i < length; i++)
+    {
+        symbol = expression[i];
+        if (isDigit(symbol))
+        {   
+            push(symbol - '0');
+        }
+        else
+        {
+            op2 = pop();
+            op1 = pop();
+            res = evaluate(op1,op2,symbol);
+
+            push(res);
+        }
+        
+    }
+    res = pop();
+    printf("\nResult : %lf",res);
+}
